49b5e7c5ce4b3e97a465bff3a82564a257487e34
Barak Holtzer <barakeholtzer@gmail.com>
2026-09-22 15:41:24 +0300

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,9 @@ android {
     defaultConfig {
         applicationId = "com.bihstudio.uvindex"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.4"
+        targetSdk = 36
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
